01f0ab3ff362ec31e3b4e12d5f671968c23bc49b
endah129 <endahkomariahlestari@gmail.com>
2024-12-17 10:49:35 +0800

    perubahan endah

--- lib/pembayaran/dataPembayaran.dart
+++ lib/pembayaran/dataPembayaran.dart
@@ -3,7 +3,7 @@ import 'package:mob3_uas_klp_06/pembayaran/inputPembayaran.dart';
 
 class DataPembayaranScreen extends StatelessWidget {
   const DataPembayaranScreen({super.key});
-
+// up
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -14,7 +14,8 @@ class DataPembayaranScreen extends StatelessWidget {
           backgroundColor: Colors.blue.shade600,
           bottom: const TabBar(
             labelColor: Colors.white, // Warna teks untuk tab yang dipilih
-            unselectedLabelColor: Colors.white70, // Warna teks untuk tab yang tidak dipilih
+            unselectedLabelColor:
+                Colors.white70, // Warna teks untuk tab yang tidak dipilih
             indicatorColor: Colors.white, // Warna indikator di bawah tab
             tabs: [
               Tab(text: 'Data Pembayaran'), // Tab kiri
